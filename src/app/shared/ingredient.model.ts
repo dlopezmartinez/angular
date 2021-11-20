f9859d481961f4c747d2ebb3aca9a581ec3e74c5
Daniel <danisangonera1996@gmail.com>
2021-11-20 17:57:24 +0100

    Shopping web
    Commit inicial

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class Ingredient {
+  // public name : string;
+  // public amount : number;
+  //éste constructor tiene la misma función que si dejasemos todo lo demás aquí comentado
+  constructor(public name: string , public amount : number){}
+
+  // constructor(name : string , amount : number){
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+
+
+}
