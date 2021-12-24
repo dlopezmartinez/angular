f8e936f78ad571daa794c6fb4462f12ebf0a2a0b
Daniel <danisangonera1996@gmail.com>
2021-12-24 12:48:38 +0100

    Directives initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shopping';
-  view: number = 0;
-
-
-
-  showView(event: { viewType: string }) {
-
-    if (event.viewType === 'recipes')
-      this.view = 1;
-
-    if (event.viewType === 'shopping')
-      this.view = 2;
-
-    if (event.viewType === 'home')
-      this.view = 0;
-
-  }
-
-
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipesItemComponent } from './recipes/recipes-item/recipes-item.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipesListComponent,
-    RecipesDetailComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    RecipesItemComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
