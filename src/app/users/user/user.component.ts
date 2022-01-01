cb51d341f6be2f698d31eccae1f00c24b158ed8e
Daniel <danisangonera1996@gmail.com>
2022-01-02 00:20:03 +0100

    Routing params and observables.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number, name: string };
+  paramsSubs: Subscription;
+
 
   constructor(private route: ActivatedRoute) { }
 
@@ -16,7 +19,20 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name'],
     };
-    console.log(this.route.params['id']);
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name']; //params a diferencia de snapshot está hecho a base de Observables, lo que nos permite reaccionar a cambios en la ruta. Las subscripciones que hagamos a un observable no se borran a diferencia de los componentes.
+        }
+      );
   }
+
+
+  ngOnDestroy(): void {
+    this.paramsSubs.unsubscribe(); //no es necesario realizar ésto ya que no afecta al rendimiento de nuestra app, pero en caso de que queramos desubscribirnos cuando el componente es destruído, ésta sería la manera.
+  }
+
+
 }
 
