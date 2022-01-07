9329ad3ad45fc74902a8a4e69413b724068e95ef
Daniel <danisangonera1996@gmail.com>
2022-01-07 18:53:36 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,51 +7,4 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
 
-  @ViewChild('f') signupForm: NgForm; //otra manera de tener acceso a los forms, incluso antes de que hagan submit.
-  defaultQuestion = 'pet'
-  answer = '';
-  genders = ['male', 'female', 'helicoptero apache de combate']
-  defaultGender = 'helicoptero apache de combate';
-  submitted = false;
-  user = {
-    username: '',
-    email: '',
-    question: '',
-    answer: '',
-    gender: ''
-  }
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    this.signupForm.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
-    })
-    // this.form.setValue({
-    //   userData: {
-    //     username: suggestedName,
-    //     email: ''
-    //   },
-    //   secret: 'pet',
-    //   questionAnswer: '',
-    //   gender: 'male'
-    // })
-  }
-
-
-  // onSubmit(form: NgForm) {
-  //   console.log(form);
-  // }
-
-  onSubmit() {
-    console.log(this.signupForm.value);
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.question = this.signupForm.value.secret;
-    this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
-    this.submitted = true;
-    this.signupForm.reset();
-  }
 }
