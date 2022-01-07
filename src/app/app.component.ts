2e36c1de6912e6f66c83d822d9ffc85400ba3560
Daniel <danisangonera1996@gmail.com>
2022-01-07 20:01:52 +0100

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,5 +15,7 @@ export class AppComponent {
   onSubmit(form: NgForm): void {
     console.log(form.value);
     console.log(this.defaultSub);
+    let data = form.value;
+    form.form.reset(data); //reseteo el formulario al mismo valor que tenía anteriormente, de forma que se puede ver con inspeccionar elemento como aparecen las css de ng-untouched
   }
 }
