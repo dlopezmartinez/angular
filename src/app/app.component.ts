891ad6dec8cca54f7ea7856e194e9187b0a5e3e8
Daniel <danisangonera1996@gmail.com>
2022-01-09 00:47:10 +0100

    bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,15 +16,40 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [this.forbiddenNames.bind(this), Validators.required]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
       }),
       'gender': new FormControl('male', Validators.required),
       'hobbies': new FormArray([])
     });
+
+    // this.signupForm.valueChanges.subscribe((value) => {
+    //   console.log(value);
+    // })
+
+    this.signupForm.statusChanges.subscribe((value) => {
+      console.log(value);
+    })
+
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Daniel',
+        'email': 'mierda@gmail.com'
+      },
+      'gender': 'female',
+      'hobbies': []
+    })
+
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Danieeeeeeeel'
+      }
+    })
+
   }
 
   onSubmit() {
     console.log(this.signupForm.value);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
@@ -36,11 +62,23 @@ export class AppComponent implements OnInit {
   }
   //{ [s: string]: boolean } ésto es una forma de con TS crear un objecto de clave-valor admitido por JS donde obligas a que la key sea de tipo string
   forbiddenNames(control: FormControl): { [s: string]: boolean } {
-
     if (this.forbiddenUsernames.includes(control.value)) {
       return { 'nameIsForbidden': true }
     }
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ 'emailIsForbidden': true });
+        } else {
+          resolve(null)
+        }
+      }, 1500)
+    })
+    return promise;
   }
 
 }
