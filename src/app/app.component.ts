0e22f510b8fb48c64ecb51a41319e55d44e696b7
Daniel <danisangonera1996@gmail.com>
2022-01-09 00:45:43 +0100

    Custom Validator, Async Validator, setValues, reset ....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,13 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
-
   signupForm: FormGroup;
+  forbiddenUsernames = ['Mono', 'Malo'];
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [this.forbiddenNames.bind(this), Validators.required]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male', Validators.required),
@@ -33,6 +33,14 @@ export class AppComponent implements OnInit {
 
   getHobbiesControls() {
     return (<FormArray>this.signupForm.get('hobbies')).controls
+  }
+  //{ [s: string]: boolean } ésto es una forma de con TS crear un objecto de clave-valor admitido por JS donde obligas a que la key sea de tipo string
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+
+    if (this.forbiddenUsernames.includes(control.value)) {
+      return { 'nameIsForbidden': true }
+    }
+    return null;
   }
 
 }
